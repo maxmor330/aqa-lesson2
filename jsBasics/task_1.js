7390767c155944054de9d80d6e7119a4a10198ec
maxmor330 <maxmor330@gmail.com>
2026-03-25 21:47:54 +0200

    homework4

---
+++ jsBasics/task_1.js
@@ -0,0 +1,19 @@
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних:
+// рядок, число, буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+const str = "just sting";
+console.log(str);
+
+const int = 777;
+console.log(int);
+
+const bool = true;
+console.log(bool);
+
+const n0ll = null;
+console.log(n0ll);
+
+const undef1 = undefined;
+console.log(undef1);
+let undef2 
+console.log(undef2);
