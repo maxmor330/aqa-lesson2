e9c67a91b326996303f26e210eb348f7d00e7970
maxmor330 <maxmor330@gmail.com>
2026-04-26 22:44:53 +0300

    homework8

---
+++ hw8_arrays/task6.js
@@ -0,0 +1,21 @@
+/*
+У вас є вихідний масив
+сonst numbersList = [1,10,14,2,4,5,43,34]
+Створіть новий масив на основі вихідного масиву (копіюйте його)
+Відсортуйте створений масив (від меншого до більшого)
+Виведіть обидва масиви в консоль
+*/
+
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const copiedList = numbersList.slice();
+copiedList.sort((a, b) => a - b);
+
+console.log(numbersList);
+console.log(copiedList);
+
+// one more option
+const list = [1, 10, 14, 2, 4, 5, 43, 34];
+const sortedList = list.toSorted((a, b) => a - b);
+
+console.log(list);
+console.log(sortedList);
